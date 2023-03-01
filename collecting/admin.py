bbf1910e56cbd6628c3269a29d4d67183183ce4c
Daikku <vadess2013@mail.ru>
2023-03-01 22:55:34 +0300

    Display models in admin-panel

--- collecting/admin.py
+++ collecting/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from collecting.models import City, Language, Vacancy
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'city', 'language')
+    list_display_links = ('id', 'title')
+    list_filter = ('city__name', 'language__name')
