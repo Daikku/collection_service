252b7bbf30c224344adcf37a01b64562a5b112b6
Daikku <vadess2013@mail.ru>
2023-03-01 22:54:29 +0300

    Configuring the app display

---
+++ collecting/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'collecting.apps.CollectingConfig'
--- collecting/apps.py
+++ collecting/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CollectingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'collecting'
+    verbose_name = 'Приложение для сбора'
