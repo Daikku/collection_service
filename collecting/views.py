2aeb531d046a60b7ded1a116ec5516931c9a6f07
Daikku <vadess2013@mail.ru>
2023-03-06 23:37:25 +0300

    Modifier function get vacancies

--- collecting/views.py
+++ collecting/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+from .forms import CollectionForm
 from .models import Vacancy
 
 
 def home_page(request):
-    vacancies = Vacancy.objects.all()
-    return render(request, 'collecting/home_page.html', {'vacancies': vacancies})
+    form = CollectionForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    vacancies = list()
+    if city or language:
+        _filter = dict()
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+
+        vacancies = Vacancy.objects.filter(**_filter)
+    return render(request, 'collecting/home_page.html', {'vacancies': vacancies, 'form': form})
