4a8777130fa71af18f48c88e7c022d2e9bba5c67
Daikku <vadess2013@mail.ru>
2023-03-07 18:54:20 +0300

    Added exclude field for a model CustomUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,3 +7,5 @@ from .models import CustomUser
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username')
     list_display_links = ('id', 'username')
+    exclude = ('password', 'last_login', 'groups', 'user_permissions',
+               'is_superuser', 'is_active', 'is_staff', 'date_joined')
