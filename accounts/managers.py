1f218617a9a1f5c4a58727c7ab8eda0e233756fa
Daikku <vadess2013@mail.ru>
2023-03-12 21:17:26 +0300

    Updated comment a file managers.py

--- accounts/managers.py
+++ accounts/managers.py
@@ -7,17 +7,10 @@ class UserManager(BaseUserManager):
 
     def _create_user(self, username, password, **extra_fields):
         """
-        Create and save a user with the given username, email, and password.
+        Создание и сохранение пользователя с переданными именем пользователя и паролем
         """
         if not username:
             raise ValueError("The given username must be set")
-        # Lookup the real model class from the global app registry so this
-        # manager method can be used in migrations. This is fine because
-        # managers are by definition working on the real model.
-        #GlobalUserModel = apps.get_model(
-       #     self.model._meta.app_label, self.model._meta.object_name
-        #)
-        #username = GlobalUserModel.normalize_username(username)
         user = self.model(username=username, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
