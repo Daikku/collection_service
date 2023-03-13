086bcad114fa8702d61f730ef98b190bb04cdfaf
Daikku <vadess2013@mail.ru>
2023-03-13 22:58:37 +0300

    Connected django setup to write information to databases

--- run_parsers.py
+++ run_parsers.py
@@ -1,5 +1,17 @@
 import codecs
+import os
+import sys
+import django
+
+project_path = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(project_path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django.setup()
+
 from collecting.parsers import *
+from collecting.models import Vacancy, City, Language
+
 
 parsers = (
     (parse_hhru, 'https://hh.ru/search/vacancy?text=Python&area=1'),
@@ -7,6 +19,7 @@ parsers = (
 )
 
 works, errors = list(), list()
+city = City.objects.filter(slug='moskva')
 
 for function, url in parsers:
     w, err = function(url)
