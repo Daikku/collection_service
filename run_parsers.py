fb66363c129f87b02a1f006637eb648ee785dbb8
Daikku <vadess2013@mail.ru>
2023-04-05 18:57:33 +0300

    Improved file run_parsers.py and added writing to the database.

--- run_parsers.py
+++ run_parsers.py
@@ -1,17 +1,18 @@
 import codecs
 import os
 import sys
-import django
+from django.db import DatabaseError
 
 project_path = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(project_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+import django
+
 django.setup()
 
 from collecting.parsers import *
-from collecting.models import Vacancy, City, Language
-
+from collecting.models import Vacancy, City, Language, Error
 
 parsers = (
     (parse_hhru, 'https://hh.ru/search/vacancy?text=Python&area=1'),
@@ -19,12 +20,23 @@ parsers = (
 )
 
 works, errors = list(), list()
-city = City.objects.filter(slug='moskva')
+city = City.objects.filter(slug='moskva').first()
+language = Language.objects.filter(slug='python').first()
 
 for function, url in parsers:
     w, err = function(url)
-    works.append(w)
-    errors.append(err)
+    works += w
+    errors += err
 
-with codecs.open('work.txt', 'w', 'utf-8') as file:
-    file.write(str(works))
+for work in works:
+    vac = Vacancy(**work, city=city, language=language)
+    try:
+        vac.save()
+    except DatabaseError:
+        print('ОШИБКА!!! Данные не сохранены в базе.')
+
+if errors:
+    error = Error(errors).save()
+
+# with codecs.open('work.txt', 'w', 'utf-8') as file:
+# file.write(str(works))
