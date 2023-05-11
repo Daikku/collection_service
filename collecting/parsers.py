7b05aa41204c7d4a19f98ac19ba84be116599ee7
Daikku <vadess2013@mail.ru>
2023-05-11 20:54:26 +0300

    Updated functions for parsing by city and programming language

--- collecting/parsers.py
+++ collecting/parsers.py
@@ -16,51 +16,55 @@ HEADERS = [
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'},
     ]
 
-def parse_hhru(url: str):
+
+def parse_hhru(url: str, city=None, language=None):
     works = list()
     errors = list()
     response = requests.get(url, headers=choice(HEADERS))
-    if response.status_code == 200:
-        soup = bs(response.content, 'lxml')
-        main_div = soup.find('div', id='a11y-main-content')
-        if main_div:
-            items_div = main_div.find_all('div', class_='vacancy-serp-item__layout')
-            for item in items_div:
-                title = item.find('h3')
-                url_vacancy = title.a.get('href')
-                company_div = item.find('div', class_='vacancy-serp-item__meta-info-company')
-                company = company_div.a.text
-                city = item.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
-                works.append({'title': title.text, 'url': url_vacancy, "company": company, 'city': city})
+    if url:
+        if response.status_code == 200:
+            soup = bs(response.content, 'lxml')
+            main_div = soup.find('div', id='a11y-main-content')
+            if main_div:
+                items_div = main_div.find_all('div', class_='vacancy-serp-item__layout')
+                for item in items_div:
+                    title = item.find('h3')
+                    url_vacancy = title.a.get('href')
+                    company_div = item.find('div', class_='vacancy-serp-item__meta-info-company')
+                    company = company_div.a.text
+                    works.append({'title': title.text, 'url': url_vacancy, "company": company,
+                                  'city_id': city, 'language_id': language})
+            else:
+                errors.append({'url': url, 'title': 'ERROR: Div not found'})
         else:
-            errors.append({'url': url, 'title': 'ERROR: Div not found'})
-    else:
-        errors.append({'url': url, 'title': 'ERROR: Page do not response', 'status': response.status_code})
+            errors.append({'url': url, 'title': 'ERROR: Page do not response', 'status': response.status_code})
 
     return works, errors
 
-def parse_rabotaru(url: str):
+
+def parse_rabotaru(url: str, city=None, language=None):
     works = list()
     errors = list()
     domain = 'https://www.rabota.ru/'
-    response = requests.get(url, headers=choice(HEADERS))
-    if response.status_code == 200:
-        soup = bs(response.content, 'lxml')
-        main_div = soup.find('div', class_='home-vacancies__infinity-list')
-        if main_div:
-            items_div = main_div.find_all('div', class_='vacancy-preview-card__top')
-            for item in items_div:
-                title = item.find('h3')
-                url_vacancy = title.a.get('href')
-                description = item.find('div', attrs={'itemprop': 'description'}).text
-                company_div = item.find('span', class_='vacancy-preview-card__company-name')
-                company = company_div.text.strip()
-                city = item.find('span', attrs={'itemprop': 'address'}).text.strip()
-                works.append({'title': title.text.strip(), 'url': domain + url_vacancy, 'description': description,
-                              "company": company, 'city': city})
+    if url:
+        response = requests.get(url, headers=choice(HEADERS))
+        if response.status_code == 200:
+            soup = bs(response.content, 'lxml')
+            main_div = soup.find('div', class_='home-vacancies__infinity-list')
+            if main_div:
+                items_div = main_div.find_all('div', class_='vacancy-preview-card__top')
+                for item in items_div:
+                    title = item.find('h3')
+                    url_vacancy = title.a.get('href')
+                    description = item.find('div', attrs={'itemprop': 'description'}).text
+                    company_div = item.find('span', class_='vacancy-preview-card__company-name')
+                    company = company_div.text.strip()
+                    #city = item.find('span', attrs={'itemprop': 'address'}).text.strip()
+                    works.append({'title': title.text.strip(), 'url': domain + url_vacancy, 'description': description,
+                                  "company": company, 'city_id': city, 'language_id': language})
+            else:
+                errors.append({'url': url, 'title': 'ERROR: Div not found'})
         else:
-            errors.append({'url': url, 'title': 'ERROR: Div not found'})
-    else:
-        errors.append({'url': url, 'title': 'ERROR: Page do not response', 'status': response.status_code})
+            errors.append({'url': url, 'title': 'ERROR: Page do not response', 'status': response.status_code})
 
     return works, errors
