1543ff11d3aeb93c01d228694c944983d8bd7e6a
Daikku <vadess2013@mail.ru>
2023-05-18 17:09:23 +0300

    Created function list_view and added pagination

--- collecting/views.py
+++ collecting/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import CollectionForm
@@ -6,9 +7,13 @@ from .models import Vacancy
 
 def home_page(request):
     form = CollectionForm()
+    return render(request, 'collecting/home_page.html', {'form': form})
+
+def list_view(request):
+    form = CollectionForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    vacancies = list()
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = dict()
         if city:
@@ -17,4 +22,10 @@ def home_page(request):
             _filter['language__slug'] = language
 
         vacancies = Vacancy.objects.filter(**_filter)
-    return render(request, 'collecting/home_page.html', {'vacancies': vacancies, 'form': form})
+
+        paginator = Paginator(vacancies, 10)
+        page_number = request.GET.get('page')
+        page_object = paginator.get_page(page_number)
+
+        context['vacancies'] = page_object
+    return render(request, 'collecting/list_page.html', context=context)
